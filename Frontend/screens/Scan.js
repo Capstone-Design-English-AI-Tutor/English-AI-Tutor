4c3910146e68a2edc2284d5b26f31a168d785bdd
komg00 <komg00@naver.com>
2024-05-23 05:22:31 +0900

    Feat: 이미지 업로드 & 미리보기 구현

--- Frontend/screens/Scan.js
+++ Frontend/screens/Scan.js
@@ -16,9 +16,10 @@ import * as ImagePicker from "expo-image-picker";
 function Scan() {
   const [image, setImage] = useState(null);
   const [result, setResult] = useState(null);
+  const [text, setText] = useState(null);
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
+    // 추후 보완 예정
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -33,40 +34,32 @@ function Scan() {
     }
   };
 
-  const handleGalleryPress = () => {
-    const options = {
-      mediaType: "photo",
-      includeBase64: true,
-    };
+  const handleGalleryPress = async () => {
+    console.log("image", image);
+    if (!image) {
+      Alert.alert("No image selected", "Please select an image first.");
+      return;
+    }
 
-    launchImageLibrary(options, async (response) => {
-      if (response.didCancel) {
-        console.log("User cancelled image picker");
-      } else if (response.errorCode) {
-        console.log("ImagePicker Error: ", response.errorMessage);
-      } else if (response.assets && response.assets.length > 0) {
-        const image = response.assets[0];
-
-        const formData = new FormData();
-        formData.append("image", {
-          uri: image.uri,
-          type: image.type,
-          name: image.fileName,
-        });
-
-        try {
-          const res = await axios.post("http://localhost:8000/ocr", formData, {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          });
-          setResult(res.data);
-        } catch (error) {
-          console.error("Error uploading file:", error);
-          Alert.alert("Error", "There was an error uploading the file.");
-        }
-      }
+    let formData = new FormData();
+    formData.append("image", {
+      uri: image.uri,
+      type: image.mime,
+      name: image.fileName,
     });
+
+    try {
+      const res = await axios.post("http://10.0.2.2:8000/ocr", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setText(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      Alert.alert("Error", "There was an error uploading the file.");
+    }
   };
 
   return (
@@ -75,14 +68,18 @@ function Scan() {
         <Image source={Camera} style={styles.image} />
         <Text style={styles.text}>카메라로 찍기</Text>
       </View>
-      <TouchableOpacity style={styles.content} onPress={handleGalleryPress}>
+      <TouchableOpacity style={styles.content} onPress={pickImage}>
         <Image source={Gallery} style={styles.image} />
         <Text style={styles.text}>갤러리에서 불러오기</Text>
       </TouchableOpacity>
       <View style={styles.image_container}>
-        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        <Button
+          title="Pick an image from camera roll"
+          onPress={handleGalleryPress}
+        />
         {image && <Image source={{ uri: image }} style={styles.image} />}
       </View>
+      {text && <Text>{JSON.stringify(text, null, 2)}</Text>}
     </View>
   );
 }
@@ -101,8 +98,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 25,
   },
   image: {
-    width: 120,
-    height: 120,
+    width: 20,
+    height: 20,
     marginBottom: 10,
   },
   text: {
