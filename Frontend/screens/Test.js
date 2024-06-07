d420fa75ffbac3dab84a05e7e8246b3e15d4fee0
komg00 <komg00@naver.com>
2024-06-08 01:51:02 +0900

    Feat: 영단어 테스트 보완

--- Frontend/screens/Test.js
+++ Frontend/screens/Test.js
@@ -3,16 +3,33 @@ import {
   View,
   Text,
   TextInput,
-  Alert,
   FlatList,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
 import { TestContext } from "../context/TestContext";
 import Toast from "react-native-toast-message";
+import * as Progress from "react-native-progress";
+import { useNavigation } from "@react-navigation/native";
+
+const toastConfig = {
+  success: ({ text1, text2 }) => (
+    <View style={styles.toastSuccess}>
+      <Text style={styles.toastText1}>{text1}</Text>
+      <Text style={styles.toastText2}>{text2}</Text>
+    </View>
+  ),
+  error: ({ text1, text2 }) => (
+    <View style={styles.toastError}>
+      <Text style={styles.toastText1}>{text1}</Text>
+      <Text style={styles.toastText2}>{text2}</Text>
+    </View>
+  ),
+};
 
 function Test() {
   const { quizList } = useContext(TestContext);
+  const navigation = useNavigation();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [userAnswer, setUserAnswer] = useState("");
   const [results, setResults] = useState([]);
@@ -34,19 +51,18 @@ function Test() {
     if (isCorrect) {
       setScore(score + 1);
       Toast.show({
-        type: 'success',
-        text1: '정답입니다!',
+        type: "success",
+        text1: "정답입니다!",
         text2: `${currentQuiz.quiz} : ${currentQuiz.answer}`,
-        top: 200,
         visibilityTime: 2000, // 2초 동안 표시
         autoHide: true,
       });
     } else {
       Toast.show({
-        type: 'error',
-        text1: '오답입니다.',
+        type: "error",
+        text1: "오답입니다.",
         text2: `올바른 답은 ${currentQuiz.answer}입니다.`,
-        top: 200,
+        visibilityTime: 2000,
         autoHide: true,
       });
     }
@@ -54,22 +70,27 @@ function Test() {
     setCurrentIndex(currentIndex + 1);
   };
 
+  const handleExit = () => {
+    setCurrentIndex(quizList.length);
+  };
+
   if (currentIndex >= quizList.length) {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>테스트 결과</Text>
-
-        <Text>
-          점수: {score} / {quizList.length}
+        <Text style={styles.resultTitle}>문제 풀이 결과</Text>
+        <Text style={styles.resultText}>
+          맞은 단어: {score} / {quizList.length}
         </Text>
         <FlatList
           data={results}
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
             <View style={styles.resultItem}>
-              <Text>한글: {item.quiz}</Text>
-              <Text>정답: {item.answer}</Text>
-              <Text>내가 적은 답: {item.userAnswer}</Text>
+              <Text style={styles.answer}>{item.answer}</Text>
+              <Text style={styles.quiz}>{item.quiz}</Text>
+              <Text style={styles.userAnswer}>
+                내가 적은 답: {item.userAnswer}
+              </Text>
               {item.isCorrect ? (
                 <Text style={styles.correct}>맞은 단어</Text>
               ) : (
@@ -78,13 +99,49 @@ function Test() {
             </View>
           )}
         />
-        <Toast ref={(ref) => Toast.setRef(ref)} />
+        <TouchableOpacity
+          style={styles.home}
+          onPress={() => navigation.navigate("Scan")}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 16,
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            홈으로 가기
+          </Text>
+        </TouchableOpacity>
+        <Toast config={toastConfig} ref={(ref) => Toast.setRef(ref)} />
       </View>
     );
   }
 
+  const progress = currentIndex / quizList.length;
+
   return (
     <View style={styles.container}>
+      <View style={styles.progresContainer}>
+        <Progress.Bar
+          progress={progress}
+          width={null}
+          height={12}
+          color={"#2D31A6"}
+          style={styles.progress}
+          borderRadius={16}
+          flex={1}
+          marginHorizontal={10}
+        />
+        <Text>
+          {currentIndex}/{quizList.length}
+        </Text>
+        <TouchableOpacity style={styles.close} onPress={handleExit}>
+          <Text style={{ fontSize: 15, color: "white" }}>종료</Text>
+        </TouchableOpacity>
+      </View>
+
       <Text style={styles.quizText}>{quizList[currentIndex].quiz}</Text>
       <TextInput
         style={styles.input}
@@ -98,7 +155,7 @@ function Test() {
       >
         <Text style={styles.text}>제출</Text>
       </TouchableOpacity>
-      <Toast ref={(ref) => Toast.setRef(ref)} />
+      <Toast config={toastConfig} ref={(ref) => Toast.setRef(ref)} />
     </View>
   );
 }
@@ -108,27 +165,58 @@ export default Test;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     padding: 16,
   },
+  progresContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
+  },
+  close: {
+    backgroundColor: "#F88600",
+    borderRadius: 8,
+    padding: 10,
+    marginLeft: 10,
+  },
   quizText: {
-    fontSize: 18,
+    fontSize: 22,
     textAlign: "left",
-    marginBottom: 16,
+    marginBottom: 20,
     marginLeft: 20,
+    marginTop: 20,
+    fontWeight: "bold",
   },
   input: {
-    height: 40,
+    height: 50,
     borderColor: "gray",
     borderWidth: 1,
     marginBottom: 16,
     marginHorizontal: 15,
-    paddingHorizontal: 8,
+    paddingHorizontal: 12,
+    fontSize: 17,
   },
   resultItem: {
-    padding: 16,
-    borderBottomColor: "gray",
-    borderBottomWidth: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 20,
+    backgroundColor: "#F4F3F6",
+    marginVertical: 7,
+    display: "flex",
+    flexDirection: "column",
+  },
+  answer: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  quiz: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  userAnswer: {
+    fontSize: 16,
+    marginBottom: 5,
   },
   button: {
     width: 60,
@@ -146,8 +234,47 @@ const styles = StyleSheet.create({
   },
   correct: {
     color: "#45C486",
+    alignSelf: "flex-end",
   },
   incorrect: {
     color: "#EB3223",
+    alignSelf: "flex-end",
+  },
+  toastSuccess: {
+    width: 350,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "#C6F6D5",
+  },
+  toastError: {
+    width: 350,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: "#FEEBCB",
+  },
+  toastText1: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  toastText2: {
+    fontSize: 15,
+    color: "#4F4F4F",
+  },
+  resultTitle: {
+    marginBottom: 7,
+    fontSize: 21,
+    fontWeight: "bold",
+  },
+  resultText: {
+    marginBottom: 7,
+    fontSize: 18,
+  },
+  home: {
+    paddingVertical: 15,
+    backgroundColor: "#1849A9",
+    marginTop: 15,
+    textAlign: "center",
+    borderRadius: 8,
   },
 });
